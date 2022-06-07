fb4f65fae99540d5cf3427d4c5f7a13df4f63c10
BitSoftwareCo <74556793+BitSoftwareCo@users.noreply.github.com>
2022-06-06 20:39:22 -0400

    6.6.22@0.0.1

--- src/page/App.js
+++ src/page/App.js
@@ -1,12 +1,65 @@
 import packageJson from '../../package.json';
 
-
 function App() {
+  let currentID = 0;
+
+
+  function newRecipe(e) {
+
+    const sRecipeName = document.getElementById('recipeName').value;
+    const sRecipeDescription = document.getElementById('recipeDescription').value;
+    const iCookTime = document.getElementById('cookTime').value;
+    const sRecipeSteps = document.getElementById('recipeSteps').value;
+    const sRecipeIngredients = document.getElementById('recipeIngredients').value;
+
+    let aRecipeSteps = sRecipeSteps.split(',');
+    let aRecipeIngredients = sRecipeIngredients.split(',');
+
+    let jRecipe = {
+      id: currentID,
+      recipeName: sRecipeName,
+      recipeDescription: sRecipeDescription,
+      cookTime: iCookTime,
+      recipeSteps: aRecipeSteps,
+      recipeIngredients: aRecipeIngredients,
+    }
+
+    currentID++;
+
+    console.log(jRecipe);
+    alert(
+      `id ${jRecipe.id} \n` + 
+      `recipeName ${jRecipe.recipeName} \n` + 
+      `recipeDescription ${jRecipe.recipeDescription} \n` + 
+      `cookTime ${jRecipe.cookTime} \n` + 
+      `recipeSteps ${jRecipe.recipeSteps} \n` + 
+      `recipeIngredients ${jRecipe.recipeIngredients}`
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="version-string">(DEV) {packageJson.name}@{packageJson.version} - 6.6.22</p>
-      </header>
+      <label>Recipe Name</label>
+      <input type='text' id='recipeName' />
+      <br></br>
+
+      <label>Recipe Description</label>
+      <input type='text' id='recipeDescription' />
+      <br></br>
+
+      <label>Cook Time (mins)</label>
+      <input type='number' id="cookTime" />
+      <br></br>
+
+      <label>Steps (seperate by just ',')</label>
+      <input type='text' id="recipeSteps" />
+      <br></br>
+
+      <label>Ingredients (seperate by just ',')</label>
+      <input type='text' id="recipeIngredients" />
+      <br></br>
+
+      <button onClick={newRecipe}>Add</button>
     </div>
   );
 }
